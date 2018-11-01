f5fd351d35b8d181d6ff0cf45414a1be0e658d71
Árni Dagur <arnidg@protonmail.ch>
2018-11-01 15:31:10 +0000

    Specify RUST_SRC_PATH environment variable

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -1,11 +1,16 @@
 # -- Path --
 export PATH="$HOME/bin:/usr/local/bin:$PATH"
 # Rust
-[[ -d $HOME/.cargo/bin ]] && export PATH="$HOME/.cargo/bin:$PATH"
+	# Cargo binaries
+	[[ -d $HOME/.cargo/bin ]] && export PATH="$HOME/.cargo/bin:$PATH"
+	# Rust source code (needed for rust-racer)
+	RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+	[[ -d $RUST_SRC_PATH ]] && export RUST_SRC_PATH
 # CUDA
 [[ -d /opt/cuda/bin ]] && export PATH="/opt/cuda/bin:$PATH"
 # Miniconda
 [[ -d $HOME/miniconda3/bin ]] && export PATH="$HOME/miniconda/bin:$PATH"
+
 
 # -- Other --
 export URHO3D_HOME="~/OpenSource/Urho3D"
