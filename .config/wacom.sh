5084a5d1081b0ddc75061a94765dbbfb16c54d89
Árni Dagur <arnidg@protonmail.ch>
2018-12-23 23:20:25 +0000

    Change Wacom Tablet settings

--- .config/wacom.sh
+++ .config/wacom.sh
@@ -12,6 +12,17 @@ else
 fi
 
 # -- Stylus --
-# xsetwacom set "$STYLUS" "Area" "144 450 4000 2250"
+# Previous areas: 144 450 4000 2250
 xsetwacom set "$STYLUS" "Area" "144 450 4800 2700"
 xsetwacom set "$STYLUS" "MapToOutput" "$DIMENSIONS"
+xsetwacom set "$STYLUS" "Mode" "Absolute"
+
+# Set the sample window size (a sliding average sampling window) for incoming
+# tool raw data points. Default: 4, range of 1 to 20.
+xsetwacom set "$STYLUS" "RawSample" "1"
+
+# Set the delta (difference) cutoff level for further processing of incoming
+# input tool coordinates. Suppress is a tablet wide parameter; a specified delta
+# for one input tool is applied to all input tool coordinates. Do disable
+# suppression, use a level of 0. Default: 2, range of 0 to 100.
+xsetwacom set "$STYLUS" "Suppress" "0"
