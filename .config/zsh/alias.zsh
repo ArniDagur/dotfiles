27e908737b35fb7ec430e0cc8c33048d4a50dfd1
Árni Dagur <arnidg@protonmail.ch>
2019-01-31 23:15:52 +0000

    make yay use powerpill by default

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -2,6 +2,7 @@ alias vim="nvim"
 alias emacs="emacs -nw"
 alias ls="ls --color"
 alias grep="grep --color"
+alias yay="yay --pacman powerpill"
 # Config management alias; see the following for details:
 # https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotflies-git-bare-repo/
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
