6a3f67e6297c298747500f371670638c44ac21f9
Árni Dagur <arnidg@protonmail.ch>
2019-03-24 11:49:22 +0000

    Fix caret mode keybindings for QGMLWY

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -50,6 +50,7 @@ config.bind('<Space>', 'set-cmd-text -s :open')
 config.bind('<Shift-Space>', 'set-cmd-text -s :open -t')
 config.bind('<Control-Shift-Space>', 'set-cmd-text -s :open -w')
 
+# Normal
 config.bind('u', 'scroll up')
 config.bind('U', 'tab-next')
 config.bind('a', 'scroll left')
@@ -61,3 +62,17 @@ config.bind('O', 'forward')
 
 config.bind('xd', 'download-cancel') # Cancel download; ad --> xd
 config.bind('z', 'undo') # Undo; u --> z
+
+# Caret
+config.bind('U', 'scroll up', mode='caret')
+config.bind('A', 'scroll left', mode='caret')
+config.bind('E', 'scroll down', mode='caret')
+config.bind('O', 'scroll right', mode='caret')
+config.bind('a', 'move-to-prev-char', mode='caret')
+config.bind('o', 'move-to-next-char', mode='caret')
+config.bind('e', 'move-to-next-line', mode='caret')
+config.bind('u', 'move-to-prev-line', mode='caret')
+config.bind('f', 'move-to-prev-word', mode='caret')
+config.bind('b', 'move-to-next-word', mode='caret')
+config.bind('F', 'move-to-start-of-line', mode='caret')
+config.bind('B', 'move-to-end-of-line', mode='caret')
