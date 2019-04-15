c31ea22cb736aec05e70b901a46b4e4979cb89e5
Árni Dagur <arnidg@protonmail.ch>
2019-04-15 22:02:07 +0000

    Update polybar refresh rate

--- .config/polybar/nvidia-gpu.sh
+++ .config/polybar/nvidia-gpu.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{ print ""$1"""%"}'
+nvidia-smi --query-gpu=utilization.gpu,memory.used,memory.total --format=csv,noheader,nounits -l 1 | python ~/.config/polybar/nvidia-fmt.py
