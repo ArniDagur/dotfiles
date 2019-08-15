15b1407f8f3af91cc7b3e69b2c782cb598b49178
Árni Dagur <arni@dagur.eu>
2019-08-15 22:15:49 +0000

    Use diff's --color argument by default

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -2,6 +2,7 @@ alias vim="nvim"
 alias emacs="emacs -nw"
 alias ls="ls --color"
 alias grep="grep --color"
+alias diff="diff --color"
 alias yay="yay --pacman powerpill"
 # Config management alias; see the following for details:
 # https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotflies-git-bare-repo/
